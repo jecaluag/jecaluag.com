04d2171aa498b42c31cfc662a2bf5567cc6e9881
heisenbergbrb <47139738+heisenbergbrb@users.noreply.github.com>
2022-07-17 00:25:01 +0800

    Fix magic cursor package types issue

--- components/MagicCursor.tsx
+++ components/MagicCursor.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
+import { ComponentType } from 'react';
 
-const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
+const AnimatedCursor: ComponentType<any> = dynamic(() => import('react-animated-cursor'), {
   ssr: false
 });
 
