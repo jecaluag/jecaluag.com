9bfa9507cb80ab9829e664ad792bee0f1620a060
heisenbergbrb <47139738+heisenbergbrb@users.noreply.github.com>
2022-07-16 11:22:02 +0800

    Initail Commit

---
+++ components/Layout.tsx
@@ -0,0 +1,33 @@
+import Head from "next/head"
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+const Layout = ({ children }: LayoutProps): JSX.Element => {
+  return (
+    <main>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Erniemar's homepage" />
+        <meta name="author" content="Erniemar Caluag" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name="twitter:title" content="Erniemar Caluag" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@jecaluag" />
+        <meta name="twitter:creator" content="@jecaluag" />
+        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:site_name" content="Erniemar Caluag" />
+        <meta name="og:title" content="Erniemar Caluag" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <title>Erniemar Caluag - Homepage</title>
+      </Head>
+
+      <div>{children}</div>
+    </main>
+  )
+}
+
+export default Layout
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Fonts from '../components/Fonts'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Fonts />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
 
 export default MyApp
