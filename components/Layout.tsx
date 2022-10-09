945b8e387b7d90657c9ce776d00fe92a9d5d3017
heisenbergbrb <47139738+heisenbergbrb@users.noreply.github.com>
2022-10-09 23:34:08 +0800

    Added correct meta tag values

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,23 +8,29 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
     <main className="relative bg-neutral-900">
-      {/* bg-[#1F1F1F] */}
+      {/* bg-[#1F1F1F] bg-neutral-900 */}
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Erniemar's homepage" />
-        <meta name="author" content="Erniemar Caluag" />
-        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <meta name="description" content="John Erniemar Caluag - Front End Software Engineer" />
+        <meta name="author" content="John Erniemar Caluag" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="twitter:title" content="Erniemar Caluag" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@jecaluag" />
         <meta name="twitter:creator" content="@jecaluag" />
-        {/* <meta name="twitter:image" content="https://www.craftz.dog/card.png" /> */}
         <meta property="og:site_name" content="Erniemar Caluag" />
         <meta name="og:title" content="Erniemar Caluag" />
         <meta property="og:type" content="website" />
-        {/* <meta property="og:image" content="https://www.craftz.dog/card.png" /> */}
-        <title>Erniemar Caluag - Homepage</title>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#171717" />
+
+        <title>John Erniemar Caluag - Front End Software Engineer</title>
       </Head>
       
       <Navbar />
